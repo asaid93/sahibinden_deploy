f3eb8ef6e563afba980d62b86d4d41e87261baf8
Ahmet Said Akbulut <56682895+asaid93@users.noreply.github.com>
2021-11-26 14:58:43 +0300

    Update finalize-completeds.sh

--- programs/finalize-completeds.sh
+++ programs/finalize-completeds.sh
@@ -4,4 +4,5 @@ cat ${1}/* > ${1}.txt &&\
 cat ${1}.txt | sort -u -t"|" -k1,1 |\
 awk -F"|" 'BEGIN{OFS="|"} (NF ~ 12){$1=$1"|Daire"} {print}' | sort |\
 awk -F"|" 'BEGIN{OFS="|"} {split($NF,arr,"-"); $NF=arr[1]"|"arr[2]"|"arr[3]} {print}' |\
-awk -F"|" 'NF ~ 15 {print}' > ${1}-sort-NF-15.txt
+awk -F"|" 'NF ~ 15 {print}' |\
+awk 'BEGIN{FS=OFS="|"} $6~/\./{gsub(/\./,"",$6)}1' > ${1}-sort-NF-15.txt
